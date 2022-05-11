55226e36d54e14327d1b88de6df668ae11b37f7f
Rakshith Venkatachalapathy <rakshith.venkatachalapathy@alphaflow.com>
2022-05-10 18:44:37 -0700

    Change User model

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_05_10_013259) do
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "username", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
